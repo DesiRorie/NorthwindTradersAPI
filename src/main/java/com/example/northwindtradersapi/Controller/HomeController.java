7e22db0045538515869f8170ae3e8805837ac599
Desi Rorie <desirorie@Desis-MBP.lan>
2023-12-20 11:14:47 -0500

    done

--- src/main/java/com/example/northwindtradersapi/Controller/HomeController.java
+++ src/main/java/com/example/northwindtradersapi/Controller/HomeController.java
@@ -1,2 +1,19 @@
-package com.example.northwindtradersapi.Controller;public class HomeController {
+package com.example.northwindtradersapi.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping(path = "/" , method = RequestMethod.GET)
+
+    public String index(
+            @RequestParam(defaultValue = "USA") String country
+            )
+         {
+        return "Hello " + country ;
+    }
+
 }
